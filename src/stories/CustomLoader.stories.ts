8cc497a445534c5f81c9ab2b5e6fa10feeb688e6
Dauntless-Jacobson <dauntlesswt@gmail.com>
2024-07-18 12:17:13 -0700

    Add CustomLoader component for displaying a custom loading animation

---
+++ src/stories/CustomLoader.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+
+import type { PageProps } from './Page';
+import { CustomLoader } from '../components/CustomLoader';
+import * as HeaderStories from './Header.stories';
+
+const meta = {
+    title: 'custom-loader',
+    render: (args: PageProps) => CustomLoader.renderTag(),
+} satisfies Meta<PageProps>;
+
+export default meta;
+type Story = StoryObj<PageProps>;
+
+export const LoggedIn: Story = {
+    args: {
+        // More on composing args: https://storybook.js.org/docs/writing-stories/args#args-composition
+        ...HeaderStories.LoggedIn.args,
+    },
+};
