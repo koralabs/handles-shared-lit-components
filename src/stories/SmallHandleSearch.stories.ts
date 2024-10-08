d084e386f2983e54b38fb006e6706797b5f8f9ea
Dauntless-Jacobson <dauntlesswt@gmail.com>
2024-10-08 11:47:56 -0700

    Add SmallHandleSearch component and its styles

---
+++ src/stories/SmallHandleSearch.stories.ts
@@ -0,0 +1,32 @@
+import { html, TemplateResult } from 'lit';
+import '../web-components/SmallHandleSearch/index.js';
+
+export default {
+    title: 'Components/HandleSmallSearch',
+    component: 'select-handle',
+    argTypes: {
+    }
+};
+
+interface Story<T> {
+    (args: T): TemplateResult;
+    args?: Partial<T>;
+    argTypes?: Record<string, unknown>;
+    slot?: TemplateResult;
+
+}
+
+interface ArgTypes {
+    handleData?: Array<any>;
+    route?: string;
+    shouldRenderButtons?: boolean;
+    help?: string;
+}
+
+const Template: Story<ArgTypes> = ({ }: ArgTypes) => html`
+    <handle-small-search ></handle-small-search>
+
+ `;
+
+export const Regular = Template.bind({});
+
