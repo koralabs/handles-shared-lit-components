f5ebc5cf31a3de5a2b07f48e7df31215e50d27aa
Dauntless-Jacobson <dauntlesswt@gmail.com>
2024-10-10 10:38:02 -0700

    add args for pr

--- src/stories/SelectImages.stories.ts
+++ src/stories/SelectImages.stories.ts
@@ -5,7 +5,31 @@ export default {
     title: 'Components/SelectImages',
     component: 'select-images',
     argTypes: {
-    }
+        handleData: {
+            control: {
+                type: 'array', // or 'object' depending on how you want to define the array structure
+            },
+            description: 'Array of WalletHandle objects to be displayed in the component.',
+        },
+        route: {
+            control: {
+                type: 'text',
+            },
+            description: 'The URL route to navigate when a handle is selected.',
+        },
+        addFunction: {
+            control: {
+                type: 'function', // Use a function control or just describe how it should work
+            },
+            description: 'A function to be called during the firstUpdated lifecycle hook.',
+        },
+        slottedSearchStyling: {
+            control: {
+                type: 'text',
+            },
+            description: 'Styling for the slotted search input.',
+        },
+    },
 };
 
 interface Story<T> {
@@ -13,23 +37,48 @@ interface Story<T> {
     args?: Partial<T>;
     argTypes?: Record<string, unknown>;
     slot?: TemplateResult;
-
 }
 
 interface ArgTypes {
     handleData?: Array<any>;
     route?: string;
-    shouldRenderButtons?: boolean;
-    help?: string;
     addFunction?: Function;
+    slottedSearchStyling?: string;
 }
 
-const Template: Story<ArgTypes> = ({ }: ArgTypes) => html`
-    <select-images>
+const Template: Story<ArgTypes> = ({ handleData, route, addFunction, slottedSearchStyling }: ArgTypes) => html`
+    <select-images 
+        .handleData=${handleData} 
+        .route=${route} 
+        .addFunction=${addFunction} 
+        .slottedSearchStyling=${slottedSearchStyling}>
     </select-images>
- `;
+`;
 
 export const Regular = Template.bind({});
+Regular.args = {
+    handleData: [
+        {
+            name: 'Wallet 1',
+            image: 'ipfs://example-image-1',
+            active: true,
+            default: false,
+            policyId: 'policy-id-1',
+            hex: 'hex-1'
+        },
+        {
+            name: 'Wallet 2',
+            image: 'ipfs://example-image-2',
+            active: false,
+            default: true,
+            policyId: 'policy-id-2',
+            hex: 'hex-2'
+        }
+    ],
+    route: '/wallet/1',
+    addFunction: () => console.log('Add function called!'),
+    slottedSearchStyling: 'margin-bottom: 10px;'
+};
 
 
 
