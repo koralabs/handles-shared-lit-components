1437dfd627fda6a96ead12066f0bab99843a055c
Dauntless-Jacobson <dauntlesswt@gmail.com>
2024-10-10 15:57:30 -0700

    Refactor CustomToggle component to improve reactivity and update usage guide

--- src/web-components/CustomToggle/index.ts
+++ src/web-components/CustomToggle/index.ts
@@ -14,6 +14,14 @@ import { customElement, property, state } from 'lit/decorators.js';
  * ```html
  * <custom-select isActive="${true}"></custom-select>
  * ```
+ * ```html
+ * const handleEvent = (event: Event) => {
+ *  const toggleElement = event.currentTarget as CustomToggle;
+ *  isActive = !isActive;
+ *  toggleElement.isActive = isActive; // Set the reactive property on the custom element
+ * };
+ * ```
+ * 
  */
 
 @customElement('custom-toggle')
@@ -23,15 +31,12 @@ export class CustomToggle extends LitElement {
     render() {
 
         const fillColor = this.isActive ? 'rgba(77, 166, 255, 0.15)' : 'rgba(255, 255, 255, 0.1)';
-        const pathData = this.isActive
-            ? 'M9 15h60a10 10 0 0 1 10 10v10a10 10 0 0 1-10 10H9a10 10 0 0 1-10-10V25a10 10 0 0 1 10-10z'
-            : 'M9 15h60a10 10 0 0 1 10 10v10a10 10 0 0 1-10 10H9a10 10 0 0 1-10-10V25a10 10 0 0 1 10-10z';
         const circleCX = this.isActive ? '60' : '35';
 
         return html`
             <svg style="width:5.5rem; height: 2.5rem; display: flex; align-items: center;" viewBox="0 0 100 50" xmlns="http://www.w3.org/2000/svg">
-                <rect x="30" y="17" width="2.25rem" height=".75rem" rx="5" ry="10" fill="${fillColor}" />
-                <circle cx="${circleCX}" cy="23" r=".75rem" fill="white"/>
+                <rect x="30" y="17" width="2.25rem" height=".75rem" rx="5" ry="10" fill="${fillColor}" cursor="pointer"/>
+                <circle cx="${circleCX}" cy="23" r=".75rem" fill="white" cursor="pointer"/>
             </svg>
         `;
     }
