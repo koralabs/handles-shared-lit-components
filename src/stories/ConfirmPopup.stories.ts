80436de625e61514eac7443779dea7f65d1b9687
Dauntless-Jacobson <dauntlesswt@gmail.com>
2024-10-10 15:29:21 -0700

    Refactor MainPopup to ConfirmPopup components

--- src/stories/ConfirmPopup.stories.ts
+++ src/stories/ConfirmPopup.stories.ts
@@ -1,16 +1,17 @@
 import { html, TemplateResult } from 'lit';
-import '../web-components/MainPopup/index.js';
+import '../web-components/ConfirmPopup/index.js';
 
 export default {
-    title: 'Components/Main Popup',
-    component: 'main-popup',
+    title: 'Components/Confirm Popup',
+    component: 'confirm-popup',
     argTypes: {
         open: { control: 'boolean', description: 'Controls whether the popup is open or closed' },
         message: { control: 'text', description: 'Main message to display in the popup' },
         secondMessage: { control: 'text', description: 'Secondary message to display in the popup' },
         buttonConfirm: { control: 'text', description: 'Text for the confirm button' },
         buttonCancel: { control: 'text', description: 'Text for the cancel button' },
-        confirmCallback: { action: 'confirmed', description: 'Callback when confirm button is clicked' }
+        confirmCallback: { action: 'confirmed', description: 'Callback when confirm button is clicked' },
+        confirmCancel: { action: 'canceled', description: 'Callback when cancel button is clicked' },
     }
 };
 
@@ -27,17 +28,19 @@ interface ArgTypes {
     buttonConfirm: string;
     buttonCancel: string;
     confirmCallback: () => void;
+    confirmCancel: () => void;
 }
 
-const Template: Story<ArgTypes> = ({ open, message, secondMessage, buttonConfirm, buttonCancel, confirmCallback }: ArgTypes) => html`
-    <main-popup 
+const Template: Story<ArgTypes> = ({ open, message, secondMessage, buttonConfirm, buttonCancel, confirmCallback, confirmCancel }: ArgTypes) => html`
+    <confirm-popup 
         ?open=${open} 
         .message=${message} 
         .secondMessage=${secondMessage}
         .buttonConfirm=${buttonConfirm} 
         .buttonCancel=${buttonCancel} 
-        .confirmCallback=${confirmCallback}>
-    </main-popup>
+        .confirmCallback=${confirmCallback}
+        .confirmCancel=${confirmCancel}>
+    </confirm-popup>
 `;
 
 export const Regular = Template.bind({});
@@ -47,5 +50,6 @@ Regular.args = {
     secondMessage: "This action cannot be undone.",
     buttonConfirm: "Confirm",
     buttonCancel: "Cancel",
-    confirmCallback: () => console.log('Confirmed!')
+    confirmCallback: () => console.log('Confirmed!'),
+    confirmCancel: () => console.log('Canceled!'),
 };
--- src/web-components/ConfirmPopup/index.ts
+++ src/web-components/ConfirmPopup/index.ts
@@ -1,5 +1,6 @@
 import { LitElement, html, css } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { ConfirmPopupStyles } from './styles';
 /**
  * `MainPopup` is a custom popup component for displaying messages and handling user actions.
  * 
@@ -23,90 +24,17 @@ import { customElement, property } from 'lit/decorators.js';
  * </main-popup>
  * ```
  */
-@customElement('main-popup')
-export class MainPopup extends LitElement {
-    static styles = css`
-    .modal {
-        display: none;
-    }
-    .modal.show {
-        display: block;
-    }
-    .popup-overlay {
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        background-color: rgba(0, 0, 0, 0.5);
-        backdrop-filter: blur(10px);
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        z-index: 1000;
-    }
-
-    .popup-content {
-        display: flex;
-        flex-direction: column;
-        background-color: #1c2541;        
-        padding: 20px;
-        border-radius: 8px;
-        box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-        color: rgb(255, 255, 255);
-        text-align: center;
-    }
-    .popup-text {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-    }
-    .popup-text-wrapper{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        flex-direction: column;
-        font-size: 16px;
-        max-width: 350px;
-    }
-
-    .popup-actions {
-        display: flex;
-        margin-top: 20px;
-        justify-content: center;
-    }
-    .lit-button {
-        margin: 0 10px;
-    }
-    .popup-actions button {
-        margin: 0 10px;
-        padding: 8px 16px;
-        cursor: pointer;
-        border: none;
-        border-radius: 4px;
-        background-color: #007bff;
-        color: white;
-    }
-
-    .popup-actions button:hover {
-        background-color: #0056b3;
-    }
-
-    .popup-actions button:nth-child(2) {
-        background-color: #6c757d;
-    }
-
-    .popup-actions button:nth-child(2):hover {
-        background-color: #5a6268;
-    }
-    `;
+@customElement('confirm-popup')
+export class ConfirmPopup extends LitElement {
+    static styles = ConfirmPopupStyles;
     @property({ type: Boolean }) open = false;
     @property({ type: String }) message = '';
     @property({ type: String }) secondMessage = '';
     @property({ type: String }) buttonConfirm = '';
     @property({ type: String }) buttonCancel = '';
     @property({ type: Function }) confirmCallback: (() => void) | null = null;
+    @property({ type: Function }) confirmCancel: (() => void) | null = null;
+
 
     closePopup() {
         this.open = false;
@@ -120,6 +48,18 @@ export class MainPopup extends LitElement {
         this.confirmCallback = confirmCallback || null;
         this.open = true;
     }
+    private handleConfirm() {
+        if (this.confirmCallback) {
+            this.confirmCallback();
+        }
+        this.closePopup();
+    }
+    private handleCancel() {
+        if (this.confirmCallback) {
+            this.confirmCancel();
+        }
+        this.closePopup();
+    }
 
     render() {
         return html`
@@ -131,12 +71,12 @@ export class MainPopup extends LitElement {
                             <div class="popup-text">${this.secondMessage}</div>
                         </div>
                         <div class="popup-actions">
-                            <lit-button-small .buttonColor=${'rgba(157, 159, 177, 1)'} @click=${this.closePopup} class="lit-button">
+                            <shared-button-small .buttonColor=${'rgba(157, 159, 177, 1)'} @click=${this.handleCancel} class="lit-button">
                                 <div slot="button">${this.buttonCancel}</div>
-                            </lit-button-small>
-                            <lit-button-small .buttonColor=${'rgba(13, 221, 96, 1)'} @click=${this.handleConfirm} class="lit-button" >
+                            </shared-button-small>
+                            <shared-button-small .buttonColor=${'rgba(13, 221, 96, 1)'} @click=${this.handleConfirm} class="lit-button" >
                                 <div slot="button">${this.buttonConfirm}</div>
-                            </lit-button-small>
+                            </shared-button-small>
                         </div>
                     </div>
                 </div>
@@ -144,12 +84,6 @@ export class MainPopup extends LitElement {
         `;
     }
 
-    private handleConfirm() {
-        if (this.confirmCallback) {
-            this.confirmCallback();
-        }
-        this.closePopup();
-    }
 }
 
 
