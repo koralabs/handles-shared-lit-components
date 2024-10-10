07996c2115b96391145d6dcaefc4ba86262c1a38
Dauntless-Jacobson <dauntlesswt@gmail.com>
2024-10-10 09:58:48 -0700

    add args for master

--- src/stories/SmallHandleSearch.stories.ts
+++ src/stories/SmallHandleSearch.stories.ts
@@ -3,9 +3,13 @@ import '../web-components/SmallHandleSearch/index.js';
 
 export default {
     title: 'Components/HandleSmallSearch',
-    component: 'select-handle',
+    component: 'handle-small-search',
     argTypes: {
-    }
+        inputValue: { control: 'text' },
+        open: { control: 'boolean' },
+        search: { control: 'text' },
+        searching: { control: 'boolean' },
+    },
 };
 
 interface Story<T> {
@@ -13,20 +17,28 @@ interface Story<T> {
     args?: Partial<T>;
     argTypes?: Record<string, unknown>;
     slot?: TemplateResult;
-
 }
 
 interface ArgTypes {
-    handleData?: Array<any>;
-    route?: string;
-    shouldRenderButtons?: boolean;
-    help?: string;
+    inputValue?: string;
+    open?: boolean;
+    search?: string;
+    searching?: boolean;
 }
 
-const Template: Story<ArgTypes> = ({ }: ArgTypes) => html`
-    <handle-small-search ></handle-small-search>
-
- `;
+const Template: Story<ArgTypes> = ({ inputValue, open, search, searching }: ArgTypes) => html`
+    <handle-small-search
+        .inputValue="${inputValue}"
+        .open="${open}"
+        .search="${search}"
+        .searching="${searching}"
+    ></handle-small-search>
+`;
 
 export const Regular = Template.bind({});
-
+Regular.args = {
+    inputValue: '',
+    open: false,
+    search: '',
+    searching: false,
+};
